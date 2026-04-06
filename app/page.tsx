3125c2064e0a891cd17094df8f7ea2e3c0c8f445
iTz-TPATT <russelltrentonpatterson@gmail.com>
2026-04-06 17:18:41 -0500

    Add Masters Pool component

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MastersPool: React.FC = () => {
+  return (
+    <div>
+      <h1>Masters Pool</h1>
+      <p>This is the Masters Pool component.</p>
+      {/* Add more functionality here as needed */}
+    </div>
+  );
+};
+
+export default MastersPool;
